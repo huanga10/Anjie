5482bb73653b5bbecad6bbd7e2299ea6968a815e
Anjie <huanga10@seattleu.edu>
2018-04-26 15:28:59 -0700

    all files

---
+++ angie88data.js
@@ -0,0 +1,14 @@
+{
+  "data": [
+    {
+      "name": "Anjie Huang",
+      "email": "huanga10@seattleu.edu"
+    },
+    {
+      "name": "Eve Huang",
+      "email": "eveisacat@gmail.com"
+
+    },
+
+  ]
+}
